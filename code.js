c2e413e266390d3e863605d8867d1fcf960bd987
Hornsy96 <jakehorsnell96@gmail.com>
2023-05-06 12:08:32 +1000

    added the rest of the logic for game to work, all instances of player selection vs computer selection as well as a tie scenario

--- code.js
+++ code.js
@@ -20,10 +20,25 @@ function playGame(playerSelection, computerSelection) {
     computerSelection = getComputerChoice();
 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You Lose! Paper beats rock"
-        
+        return "You Lose! Paper beats rock";
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You Win! Rock beats scissors";
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You Win! Paper beats rock";
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You Lose! Scissors beats paper";
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You Lose! Rock beats scissors";
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You Win! Scissors beats paper";
+        } else {
+        return "It's a tie!";
+        }  
     }
     
-}
+        
+
 
 const result = playGame('rock', 'paper');
+
+console.log(result);
