18186622d5331b1854ac26bcb68ff174dac495a0
Hornsy96 <jakehorsnell96@gmail.com>
2023-05-25 20:33:59 +1000

    added on click functionality to playerChoice to select choice via button.
    added displayResult function to take result and
    alert on page who the winner was. changed game logic to
    
    readjusted playGame to work with new selection of playerChoice

--- code.js
+++ code.js
@@ -1,46 +1,76 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
 /* Logic to randomise the computers choice */
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3) + 1;
       if (random === 1) {
-        answer = 'rock';
+        return 'rock';
     } else if (random === 2) {
-        answer = 'paper';
+        return 'paper';
     } else {
-        answer = 'scissors'
+        return 'scissors'
     }    
-    return answer;
-}
+};
 
-const computerChoice = getComputerChoice();
-/*console.log(computerChoice)*/
+let playerSelection = '';
+let computerSelection = '';
+
+function isRock() {
+    playerSelection = 'rock';
+    computerSelection = getComputerChoice();
+    displayResult(playGame(playerSelection, computerSelection));
+};
+
+function isPaper() {
+    playerSelection = 'paper';
+    computerSelection = getComputerChoice();
+    displayResult(playGame(playerSelection, computerSelection));
+};
+
+function isScissors() {
+    playerSelection = 'scissors';
+    computerSelection = getComputerChoice();
+    displayResult(playGame(playerSelection, computerSelection));
+};
+
+rock.addEventListener('click', isRock);
+
+paper.addEventListener('click', isPaper);
+
+scissors.addEventListener('click', isScissors);
 
 
 /* This is the basic game logic */
-function playGame(playerSelection, computerSelection) {
-    const userInput = prompt('Which will you throw?');
-    playerSelection = userInput.toLowerCase();
-
-    computerSelection = getComputerChoice();
-
+function playGame(playerSelection, computerSelection) { 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You Lose! Paper beats rock";
+        return "You Lose! Paper beats rock.";
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You Win! Rock beats scissors";
+        return "You Win! Rock beats scissors.";
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You Win! Paper beats rock";
+        return "You Win! Paper beats rock.";
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return "You Lose! Scissors beats paper";
+        return "You Lose! Scissors beats paper.";
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You Lose! Rock beats scissors";
+        return "You Lose! Rock beats scissors.";
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You Win! Scissors beats paper";
+        return "You Win! Scissors beats paper.";
         } else {
         return "It's a tie!";
-        }  
-    }
+        }          
+    };
+
+    function displayResult(result) {
+        alert(`${result} Play Again?`);
+        console.log(playerSelection);
+        console.log(computerSelection);        
+    };
+
+    
     
 /* This is the function to play the game, using a loop to play bets of 5*/       
-function game() {
+/*function game() {
     let playerScore = 0;
     let computerScore = 0;
 
@@ -63,8 +93,8 @@ function game() {
         return 'You lose!'
     } else {
         return 'Tie game..'
-    }
-
-    }
+    };
+};
 
     console.log(game());
+*/
